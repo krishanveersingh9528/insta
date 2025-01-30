1d858eb4bfb12a1ed9ff77ce6587ac8fb846e981
Ansh agrawal <agrawalansh624@gmail.com>
2025-01-30 12:22:08 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+import AppRoutes from "./routes/AppRoutes"
+
+
+function App() {
+  
+
+  return (
+    <AppRoutes>
+      
+    </AppRoutes>
+  )
+}
+
+export default App
